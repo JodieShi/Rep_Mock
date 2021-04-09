72b80f91441ef9725344e663567f5e84d97e47a5
JodieShi <jodiessj@163.com>
2021-04-09 17:43:20 +0800

    Desc: update code Modifier: Jodie

--- faker/vue-antd-fa/src/utils/themeUtil.js
+++ faker/vue-antd-fa/src/utils/themeUtil.js
@@ -1,1 +1,5 @@
-const client = require('')
+const client = require('webpack-theme-color-replacer/client')
+const { theme } = require('../config')
+const { getMenuColors, getAntdColors, getThemeToggleColors, getFunctionalColors } = require('../utils/colors')
+const { ANTD } = require('../config/default')
+
